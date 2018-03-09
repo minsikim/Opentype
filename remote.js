3a3e6dea61356513adc43ec64330ec0fc1646310
minsikim <minsiua@gmail.com>
2018-03-09 19:29:36 +0900

    icon builder

--- remote.js
+++ remote.js
@@ -1,5 +1,7 @@
 const {remote} = require('electron');
-const path =require('path');
+const path = require('path');
+require('devtron').install();
+const currentWindow = remote.getCurrentWindow();
 
 document.querySelector('#new-window').addEventListener('click', ()=>{
   const win = new remote.BrowserWindow({
@@ -12,3 +14,19 @@ document.querySelector('#new-window').addEventListener('click', ()=>{
 });
 
 console.log(remote);
+
+function onBlur(){
+  document.body.style = 'opacity: 0.2;';
+}
+
+function onFocus(){
+  document.body.style = 'opacity: 1;';
+}
+
+currentWindow.on('blur', onBlur);
+currentWindow.on('focus', onFocus);
+
+window.addEventListener('beforeunload', ()=>{
+  currentWindow.removeListener('focus', onFocus);
+  currentWindow.removeListener('blur', onBlur);
+});
